a836b151be6b410755daa3be15cc75ac8087ca17
Finn <finnmortenthen@gmail.com>
2020-10-03 17:15:41 +0200

    third commit

--- project_rest_api/settings.py
+++ project_rest_api/settings.py
@@ -19,11 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e+nv@$-eliicw39fnh82m6=%f=h5)lhg$3^k9bu#jiiy+=4$s+'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
